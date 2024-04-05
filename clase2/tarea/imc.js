c5d2e8ad1db027f3e3d1bc91d08a6b695a257734
fernando <fer_oca98@hotmail.com>
2024-04-04 22:10:21 -0600

    actividad2 imc clase2

---
+++ clase2/tarea/imc.js
@@ -0,0 +1,46 @@
+function calcularIMC(peso, altura) {
+  let imc = peso / (altura * altura);
+
+  if (isNaN(imc) || !isFinite(imc)) {
+    return "Error: Los valores ingresados no son válidos.";
+  } else {
+    return imc;
+  }
+}
+
+function clasificarIMC(imc) {
+  if (imc < 18.5) {
+    return "Bajo peso";
+  } else if (imc >= 18.5 && imc < 25) {
+    return "Peso saludable";
+  } else if (imc >= 25 && imc < 30) {
+    return "Sobrepeso";
+  } else {
+    return "Obesidad";
+  }
+}
+
+//opcion 1
+// function resultado(peso, altura) {
+//   let imc = calcularIMC(peso, altura);
+//   let clasificacion = clasificarIMC(imc);
+
+//   console.log("Su IMC es:", imc);
+//   console.log("Clasificación:", clasificacion);
+// }
+
+// resultado(98, 1.82);
+
+//opcion 2
+function resultado() {
+  let peso = 98; // Kilogramos
+  let altura = 1.82; // Metros
+
+  let imc = calcularIMC(peso, altura);
+  let clasificacion = clasificarIMC(imc);
+
+  console.log("Su IMC es:", imc);
+  console.log("Clasificación:", clasificacion);
+}
+
+resultado();
