d5c6bb57f0de47f3d2a8430cdc21d184aa440152
fernando <fer_oca98@hotmail.com>
2024-04-16 20:02:12 -0600

    clase10 y actividad

--- clase10/tarea.js
+++ clase10/tarea.js
@@ -60,22 +60,43 @@ const canes = [
     4.- Necesito una lista que contenga la lista de países a los que pertenecen los perros, pero sin repetidos
     */
 
-const sumaEdades = canes.reduce((suma, perro) => suma + perro.edad, 0);
-const promedioEdades = sumaEdades / canes.length;
+//1.- Necesito conocer la edad promedio de todos los perros
+
+// const sumaEdades = canes.reduce((suma, perro) => suma + perro.edad, 0);
+// const promedioEdades = sumaEdades / canes.length;
+// console.log("Edad promedio de todos los perros:", promedioEdades);
+
+const promedioEdades =
+  canes.reduce((suma, perro) => suma + perro.edad, 0) / canes.length;
 console.log("Edad promedio de todos los perros:", promedioEdades);
 
-const perrosPorPais = {};
-canes.forEach((perro) => {
-  perrosPorPais[perro.pais] = perrosPorPais[perro.pais] || [];
-  perrosPorPais[perro.pais].push(perro);
-});
-console.log("Perros por país:", perrosPorPais);
+//2.- Necesito obtener una lista de perros basados en el país al que pertenecen
+// const perrosPorPais = canes.reduce((acumulador, perro) => {
+//   acumulador[perro.pais] = acumulador[perro.pais] || [];
+//   acumulador[perro.pais].push(perro);
+//   return acumulador;
+// }, {});
+// console.log("Perros por país:", perrosPorPais);
 
+const paisDeseado = "México";
+const perrosEnPaisDeseado = canes.filter((perro) => perro.pais === paisDeseado);
+
+if (perrosEnPaisDeseado.length === 0) {
+  console.log(`No hay perros en ${paisDeseado}`);
+} else {
+  console.log(`Perros en ${paisDeseado}:`);
+  perrosEnPaisDeseado.forEach((perro) => console.log(perro));
+}
+
+//3.- Necesito una lista de los códigos postales de los perros
 const codigosPostales = canes.map((perro) => perro.direccion.codigoPostal);
 console.log("Códigos postales de los perros:", codigosPostales);
 
-const paisesUnicos = canes.map((perro) => perro.pais);
-const paisesSinRepetidos = paisesUnicos.filter(
-  (pais, index) => paisesUnicos.indexOf(pais) === index
+//4.- Necesito una lista que contenga la lista de países a los que pertenecen los perros, pero sin repetidos
+const paisesSinRepetidos = canes.reduce(
+  (acumulador, perro) =>
+    acumulador.includes(perro.pais) ? acumulador : [...acumulador, perro.pais],
+  []
 );
+
 console.log("Países de los perros sin repetidos:", paisesSinRepetidos);
